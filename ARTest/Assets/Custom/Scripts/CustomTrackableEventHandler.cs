9fc3430ef85c2c514fe33351a25b5bb153e8d9c7
Felipe <felipe-pavanela@hotmail.com>
2019-03-26 16:44:06 +1300

    Upload 26/03

---
+++ ARTest/Assets/Custom/Scripts/CustomTrackableEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomTrackableEventHandler : DefaultTrackableEventHandler
+{
+
+    protected override void OnTrackingFound(){
+        base.OnTrackingFound();
+        
+    }
+    protected override void OnTrackingLost(){
+        base.OnTrackingLost();
+        
+        foreach (Transform t in transform) {
+            if (t.gameObject.tag == "Forklift/Body")
+            {
+                t.gameObject.AddComponent<Forklift>();
+            }
+        }
+    }
+}
---
+++ ARTest/Assets/Custom/Scripts/Forklift.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forklift : MonoBehaviour{
+    private Animator animator;
+
+    void Awake(){
+        animator = GetComponent<Animator>();
+    }
+
+    void OnMouseDown(){
+        StartCoroutine(Respond());
+    }
+
+    private IEnumerator Respond(){
+        animator.SetTrigger("Activate");
+
+        yield return null;
+    }
+}
