db95d982f5e5fe095e6abcf182e5ede5c87e0d1b
Felipe <felipe-pavanela@hotmail.com>
2019-04-01 12:08:45 +1300

    Update uni 1/4.

---
+++ CS202-AR/Assets/Custom/Scripts/CustomTrackableEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomTrackableEventHandler : DefaultTrackableEventHandler {
+    public static CustomTrackableEventHandler instance;
+
+    private void Awake() {
+        instance = this;
+    }
+
+    protected override void OnTrackingFound() {
+        base.OnTrackingFound();
+
+        Debug.LogError("<color=green>Tracking found!</color>");
+
+        // var rigidbodies = GetComponentsInChildren<Rigidbody>(true);
+        // foreach (var rb in rigidbodies) {
+        //     rb.isKinematic = false;
+        // }
+    }
+
+    protected override void OnTrackingLost() {
+        base.OnTrackingLost();
+
+        Debug.LogError("<color=red>Tracking lost!</color>");
+
+        // var rigidbodies = GetComponentsInChildren<Rigidbody>(true);
+        // foreach (var rb in rigidbodies) {
+        //     rb.isKinematic = true;
+        // }
+    }
+}
---
+++ CS202-AR/Assets/Custom/Scripts/DetectOverlap.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+    
+public class DetectOverlap : MonoBehaviour {
+    public static List<Collider> overlaps = new List<Collider>();
+    
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Interactive") {
+            Debug.Log("bhk");
+            overlaps.Add(other);
+        }
+    }
+    
+    private void OnTriggerExit(Collider other) {
+        if (other.tag == "Interactive") {
+            overlaps.Remove(other);
+        }
+    }
+}
